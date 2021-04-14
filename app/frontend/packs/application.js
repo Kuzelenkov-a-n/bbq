5aa06d3d5ae31aac90a75c85a434f8fd3fa72b6a
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-14 19:09:53 +0300

    initial commit

---
+++ app/frontend/packs/application.js
@@ -0,0 +1,11 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "../styles/application"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+// const images = require.context('../images', true)
