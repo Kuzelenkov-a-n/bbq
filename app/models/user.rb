f0afd1c3cf552e0fdb396f317a9f63bd8dd9a464
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-14 21:12:28 +0300

    AddUserToEvent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: true
 end
