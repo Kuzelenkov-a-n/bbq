916595f9e42e20eef0bf77667a583e47b64d5229
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-17 17:49:29 +0300

    modified(user edit, layout), rand user_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :events
 
   validates :name, presence: true, length: { maximum: 35 }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Товарищ №#{rand(777)}" if self.name.blank?
+  end
 end
