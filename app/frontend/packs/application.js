f6f33fc28ef2a01245790585451b98d4172f6c34
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-21 16:43:55 +0300

    mindate & timepicker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,6 +4,7 @@ import "channels"
 import "styles/application.scss"
 import "bootstrap/dist/js/bootstrap"
 import "air-datepicker/dist/js/datepicker.min"
+import "../scripts/min_date"
 
 Rails.start()
 ActiveStorage.start()
---
+++ app/frontend/scripts/min_date.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.datepicker-here').datepicker({
+        timepicker: true,
+        minDate: new Date()
+    });
+});
