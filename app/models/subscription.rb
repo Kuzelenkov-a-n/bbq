3cf2f695ccecf9ae3333b1265a52602ddd079388
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-04-28 14:39:09 +0300

    fixup validates

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,15 +4,17 @@ class Subscription < ApplicationRecord
 
   with_options if: -> { user.present? } do
     validates :user, uniqueness: { scope: :event_id }
+    validate :user_not_event_author
   end
 
   with_options unless: -> { user.present? } do
     validates :user_name, presence: true
     validates :user_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :user_email, uniqueness: { scope: :event_id }
+    validate :email_not_taken
+    validate :name_not_exist
   end
 
-  validate :email_not_taken
 
   def user_name
     if user.present?
@@ -33,8 +35,14 @@ class Subscription < ApplicationRecord
   private
 
   def email_not_taken
-    if user&.email.present? || user_email.present?
-      errors.add(:user_email, :already_exists)
-    end
+    errors.add(:user_email, :already_exists) if User.exists?(email: user_email)
+  end
+
+  def name_not_exist
+    errors.add(:user_name, :already_exists) if User.exists?(name: user_name)
+  end
+
+  def user_not_event_author
+    errors.add(:user_name, :self_event) if user.persisted?
   end
 end
