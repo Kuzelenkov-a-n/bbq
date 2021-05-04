4453fc766cdeec9a708363675fb4b5f89478ef89
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-05-04 13:30:54 +0300

    default_i18n_subject, locales, comment

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,20 +4,20 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: I18n.t('event_mailer.subscription.subject', event_title: event.title)
+    mail to: event.user.email, subject: default_i18n_subject(event_title: event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: I18n.t('event_mailer.comment.subject', event_title: event.title)
+    mail to: email, subject: default_i18n_subject(event_title: event.title)
   end
 
   def photo(event, photo, email)
     @photo = photo
     @event = event
 
-    mail to: email, subject: I18n.t('event_mailer.photo.subject', event_title: event.title)
+    mail to: email, subject: default_i18n_subject(event_title: event.title)
   end
 end
