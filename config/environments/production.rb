a67610292eeb51e817061efc6c5bbb8a21ad694b
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-05-05 13:35:43 +0300

    link_to event(event_mailer)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "bbq_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'bbqkan.herokuapp.com' }
   config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    user_name: Rails.application.credentials.gmail[:GMAIL_USERNAME],
+    password: Rails.application.credentials.gmail[:GMAIL_PASSWORD],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
