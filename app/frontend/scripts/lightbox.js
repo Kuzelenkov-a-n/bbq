515685e9e4ed5e120ac5f199eb750731cd03d9f0
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-05-07 12:19:36 +0300

    fixup arrows

--- app/frontend/scripts/lightbox.js
+++ app/frontend/scripts/lightbox.js
@@ -1,7 +1,4 @@
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
-  $(this).ekkoLightbox({
-    leftArrow: "<",
-    rightArrow: " >",
-  });
+  $(this).ekkoLightbox();
 });
