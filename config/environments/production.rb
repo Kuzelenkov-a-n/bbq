a81be39d6be72f3fcba6cd9f1f9a49b2602e495b
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-05-31 14:54:10 +0300

    correct config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "bbq_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'bbqkan.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.production[:HOST] }
   config.action_mailer.delivery_method = :mailjet
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
