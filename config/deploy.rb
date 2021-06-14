06eb421db459f33a6f96dd32c8f0c4d00d66cd8b
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-06-14 16:33:25 +0300

    fixup deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:Kuzelenkov-a-n/bbq.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "home/deploy/apps/bbq"
+set :deploy_to, "/home/deploy/apps/bbq"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
