68d513ae71c58a64b850cc5979558de687488f14
Kuzelenkov A.N <kuzelenkov.a.n@yandex.ru>
2021-06-17 11:20:21 +0300

    refactor send_email_job.rb

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -4,11 +4,10 @@ class SendEmailJob < ApplicationJob
   def perform(event, model)
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [model.user&.email]).uniq
 
-    all_emails.each do |mail|
-      EventMailer.comment(event, model, mail).deliver_later if model.class == Comment
-      EventMailer.photo(event, model, mail).deliver_later if model.class == Photo
+    case model
+    when Comment then all_emails.each { |email| EventMailer.comment(event, model, email).deliver_later }
+    when Photo then all_emails.each { |email| EventMailer.photo(event, model, email).deliver_later }
+    else EventMailer.subscription(event, model).deliver_later
     end
-
-    EventMailer.subscription(event, model).deliver_later if model.class == Subscription
   end
 end
